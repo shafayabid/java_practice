292ba61780ee2c0cb40cb3603bbe5b21ea7132a1
DESKTOP-UR9BCVV\shafay <shafayabid4@gmail.com>
2023-02-17 19:58:49 +0500

    datatypes

--- LearningJava/src/main/java/com/shafayabid/learningjava/Main.java
+++ LearningJava/src/main/java/com/shafayabid/learningjava/Main.java
@@ -3,7 +3,17 @@ package com.shafayabid.learningjava;
 public class Main {
 
     public static void main(String[] args){
+        //datatypes
+        int hello_world = 5;
+        double num2 = 5.0;
+        boolean b = true;
+        char c = 't';
+        char d = '4';
+        String str = "this is a string";
+
+        //print statements
         System.out.println("Hello World");
+        System.out.println(hello_world);
     }
 
 
