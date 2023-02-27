38e9361b9e9f3bfab4aaf02822d363dd8ccbc758
shafay abid <shafayabid4@gmail.com>
2023-02-27 19:02:44 +0500

    Constructors in the Human class

--- OOP_Concepts/src/main/java/org/example/Human.java
+++ OOP_Concepts/src/main/java/org/example/Human.java
@@ -4,6 +4,18 @@ public class Human {
     private int age;
     private String name;
 
+    //Default Constructor
+    public Human(){
+        age = 12;
+        name = "Ali";
+    }
+
+    //Parameterized Constructor
+    public Human(int age, String name){
+        this.age = age;
+        this.name = name;
+    }
+
     //getter methods
     public int getAge(){
         return age;
